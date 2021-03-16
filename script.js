c6c32792cccffd3169ba328c6dcff2b698a648d9
emanbadr100 <80752483+emanbadr100@users.noreply.github.com>
2021-03-16 12:04:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function show() {
+    if (document.getElementById("sub-menu").style.display == "none") {
+        document.getElementById("sub-menu").style.display = "inline-grid";
+    } else {
+        document.getElementById("sub-menu").style.display = "none";
+    }
+}
